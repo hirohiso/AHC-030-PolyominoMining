2107b613fe1bbfd3c13569df40612afb7dbc5751
hiro <irie.h126@gmail.com>
2024-02-10 07:59:37 +0900

    spock追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("groovy")
 }
 
 group = "net.hirohiso"
@@ -12,6 +13,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("com.google.guava:guava:27.0.1-jre")
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.8")
+    testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
 }
 
 tasks.test {
